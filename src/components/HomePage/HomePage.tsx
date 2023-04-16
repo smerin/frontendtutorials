30e1942a4130f88051d2159165574594a7da78d0
George Smerin <gsmerin@gmail.com>
2023-04-16 16:42:23 +0100

    Setup postcss plugins

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -3,7 +3,9 @@ import styles from "./HomePage.module.css";
 const HomePage = (): JSX.Element => {
   return (
     <div className={styles.homePage}>
-      <h1 className={styles.title}>Welcome to Frontend Tutorials</h1>
+      <h1 className={styles.title}>
+        Welcome to <span>Frontend</span> Tutorials
+      </h1>
       <p className={styles.message}>Website under construction</p>
     </div>
   );
