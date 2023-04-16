97b30b5d7891506e2fa959f9ef8a4c2a62f35bc7
George Smerin <gsmerin@gmail.com>
2023-04-16 07:50:44 +0100

    Basic project config

---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import styles from "./HomePage.module.css";
+
+const HomePage = (): JSX.Element => {
+  return (
+    <div className={styles.homePage}>
+      <h1 className={styles.title}>Welcome to Frontend Tutorials</h1>
+      <p className={styles.message}>Website under construction</p>
+    </div>
+  );
+};
+
+export default HomePage;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400;1,600;1,700&family=Big+Shoulders+Display:wght@300;400;900&family=Big+Shoulders+Text:wght@700&display=swap"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
