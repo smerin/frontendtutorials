69718990b83fdd2cef18d4515c790fa203f2e769
George Smerin <gsmerin@gmail.com>
2023-04-20 20:54:20 +0100

    Setup alias imports

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
+const path = require("path");
 import type { StorybookConfig } from "@storybook/nextjs";
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -20,6 +22,17 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  webpackFinal: async (config, { configType }) => {
+    if (config?.resolve?.alias) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@src": path.resolve(__dirname, "../src"),
+        "@styles": path.resolve(__dirname, "../src/styles"),
+        "@components": path.resolve(__dirname, "../src/components"),
+      };
+    }
+    return config;
   },
 };
 export default config;
